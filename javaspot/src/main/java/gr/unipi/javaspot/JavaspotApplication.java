339fd5f09eb9a50242cc92315b8c020d6814af85
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-05-15 15:43:20 +0300

    Create project
    Co-Authored-By: Anthony Roussos <anthony.rouss@proton.me>

---
+++ javaspot/src/main/java/gr/unipi/javaspot/JavaspotApplication.java
@@ -0,0 +1,13 @@
+package gr.unipi.javaspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaspotApplication.class, args);
+    }
+
+}
---
+++ javaspot/src/test/java/gr/unipi/javaspot/JavaspotApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.unipi.javaspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
