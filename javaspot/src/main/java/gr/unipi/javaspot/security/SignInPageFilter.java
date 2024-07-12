76303702cbc1a0b1988c4ed945d76581745cc16e
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-12 15:30:38 +0300

    Create SignInPageFilter.java
    - This filter intercepts requests to the /auth/signIn URL and redirects authenticated users to the /index page, preventing them from accessing the sign-in page while already authenticated.

---
+++ javaspot/src/main/java/gr/unipi/javaspot/security/SignInPageFilter.java
@@ -0,0 +1,41 @@
+package gr.unipi.javaspot.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class SignInPageFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (isAuthenticated() && request.getRequestURI().equals("/auth/signIn")) {
+            String encodedRedirectURL = response.encodeRedirectURL(request.getContextPath() + "/index");
+            response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
+            response.setHeader("Location", encodedRedirectURL);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+                isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
+    }
+
+}
