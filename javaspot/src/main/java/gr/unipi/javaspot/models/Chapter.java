41796416b9f2678246e907a14e56a501fc361b2b
Anthony Roussos <anthony.rouss@proton.me>
2024-07-19 18:59:47 +0300

    Update Chapter model
    - Add implementation for DtoConvertible
    - Add skill level property
    - Add prerequisite chapter
    - Add list of questions association

--- javaspot/src/main/java/gr/unipi/javaspot/models/Chapter.java
+++ javaspot/src/main/java/gr/unipi/javaspot/models/Chapter.java
@@ -1,16 +1,21 @@
 package gr.unipi.javaspot.models;
 
+import gr.unipi.javaspot.dtos.ChapterDTO;
+import gr.unipi.javaspot.dtos.DtoConvertible;
+import gr.unipi.javaspot.enums.SkillLevel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "chapters")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Chapter {
+public class Chapter implements DtoConvertible<ChapterDTO> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +28,24 @@ public class Chapter {
     @Column(nullable = false)
     private String description;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SkillLevel skillLevel;
+
+    @ManyToOne
+    @JoinColumn(name = "prerequisite_id")
+    private Chapter prerequisite;
+
+    @OneToMany(mappedBy = "chapter", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Question> questions;
+
+    @Override
+    public ChapterDTO toDto() {
+        return new ChapterDTO(
+                this.id,
+                this.title,
+                this.description,
+                this.skillLevel
+        );
+    }
 }
