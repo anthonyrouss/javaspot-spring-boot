baf2f3a4ba04e946f9ffd78fb94e4d110c84a1bb
Anthony Roussos <anthony.rouss@proton.me>
2024-07-19 18:57:04 +0300

    Add findByPrerequisiteId method

--- javaspot/src/main/java/gr/unipi/javaspot/repositories/ChapterRepository.java
+++ javaspot/src/main/java/gr/unipi/javaspot/repositories/ChapterRepository.java
@@ -4,6 +4,9 @@ import gr.unipi.javaspot.models.Chapter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChapterRepository extends JpaRepository<Chapter, Integer> {
+    List<Chapter> findByPrerequisiteId(int prerequisiteId);
 }
