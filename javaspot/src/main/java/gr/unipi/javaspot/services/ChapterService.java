645e58305556fbe057aa6a200af6ee3dce17fc7d
Anthony Roussos <anthony.rouss@proton.me>
2024-07-19 19:01:12 +0300

    Add getNextChapters method

--- javaspot/src/main/java/gr/unipi/javaspot/services/ChapterService.java
+++ javaspot/src/main/java/gr/unipi/javaspot/services/ChapterService.java
@@ -1,8 +1,11 @@
 package gr.unipi.javaspot.services;
 
+import gr.unipi.javaspot.dtos.ChapterDTO;
 import gr.unipi.javaspot.models.Chapter;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChapterService extends GeneralService<Chapter> {
+    List<ChapterDTO> getNextChapters(int prerequisiteId);
 }
