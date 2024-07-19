a9cefb2edf1a51e228a5d871175102c20e8771ce
Anthony Roussos <anthony.rouss@proton.me>
2024-07-19 19:01:58 +0300

    Add getChapterExams endpoint

--- javaspot/src/main/java/gr/unipi/javaspot/controllers/ChapterController.java
+++ javaspot/src/main/java/gr/unipi/javaspot/controllers/ChapterController.java
@@ -1,8 +1,11 @@
 package gr.unipi.javaspot.controllers;
 
+import gr.unipi.javaspot.enums.ExamStatus;
 import gr.unipi.javaspot.exceptions.ChapterNotFoundException;
 import gr.unipi.javaspot.models.Chapter;
+import gr.unipi.javaspot.models.Exam;
 import gr.unipi.javaspot.services.ChapterService;
+import gr.unipi.javaspot.services.ExamService;
 import gr.unipi.javaspot.services.SectionService;
 import gr.unipi.javaspot.enums.ControllerConstants;
 import lombok.AllArgsConstructor;
@@ -10,28 +13,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/chapters")
 @AllArgsConstructor
 public class ChapterController {
 
     private final ChapterService chapterService;
     private final SectionService sectionService;
+    private final ExamService examService;
 
-    @GetMapping
+    @GetMapping({"/", "/chapters"})
     public String getChapters(Model model) {
         model.addAttribute("chapters", chapterService.findAll());
         return ControllerConstants.CHAPTERS.getHtmlFilePath();
     }
 
-    @GetMapping("/{id}/sections")
+    @GetMapping("/chapters/{id}/sections")
     public String getChapterSections(Model model, @PathVariable int id) {
         Chapter selectedChapter = chapterService.getById(id).orElseThrow(() -> new ChapterNotFoundException(id));
         model.addAttribute("selectedChapterTitle", selectedChapter.getTitle());
         model.addAttribute("sections", sectionService.getAllByChapterId(id));
         return ControllerConstants.SECTIONS.getHtmlFilePath();
+    }
+
+    @GetMapping("/chapters/{id}/exams")
+    public String getChapterExams(Model model, @PathVariable int id) {
+        // TODO: Remove hardcoded username
+        Exam exam = examService.findAvailableExam("demo-user", id);
+
+        model.addAttribute("examId", exam.getId());
+        model.addAttribute("examChapterId", exam.getChapter().getId());
+        model.addAttribute("examChapterTitle", exam.getChapter().getTitle());
+        model.addAttribute("examStarted", exam.getStatus().equals(ExamStatus.IN_PROGRESS));
+        return "exams";
     }
 
 }
