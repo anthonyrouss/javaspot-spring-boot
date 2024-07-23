2d8716f8bf41bb94ea6cbc7c6280b1bcd32254b0
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-23 14:57:45 +0300

    Update AuthPagesFilter.java

--- javaspot/src/main/java/gr/unipi/javaspot/security/AuthPagesFilter.java
+++ javaspot/src/main/java/gr/unipi/javaspot/security/AuthPagesFilter.java
@@ -6,12 +6,11 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+
+import static gr.unipi.javaspot.security.SecurityConfig.isAuthenticated;
 
 public class AuthPagesFilter extends GenericFilterBean {
 
@@ -21,7 +20,8 @@ public class AuthPagesFilter extends GenericFilterBean {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         // Redirect to index page if user is authenticated and tries to access auth related endpoints
-        boolean isAuthRelatedEndpoint = request.getRequestURI().equals("/auth/signIn") || request.getRequestURI().equals("/auth/signUp");
+        String requestURI = request.getRequestURI();
+        boolean isAuthRelatedEndpoint = requestURI.equals("/auth/signIn") || requestURI.equals("/auth/signUp");
         if (isAuthenticated() && isAuthRelatedEndpoint) {
             String encodedRedirectURL = response.encodeRedirectURL(request.getContextPath() + "/index");
             response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
@@ -29,15 +29,6 @@ public class AuthPagesFilter extends GenericFilterBean {
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    private boolean isAuthenticated() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || AnonymousAuthenticationToken.class.
-                isAssignableFrom(authentication.getClass())) {
-            return false;
-        }
-        return authentication.isAuthenticated();
     }
 
 }
