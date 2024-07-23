fd402e63d83c2a16f2a870db001a6847dce271e5
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-23 19:14:52 +0300

    Update ChapterRepository.java

--- javaspot/src/main/java/gr/unipi/javaspot/repositories/ChapterRepository.java
+++ javaspot/src/main/java/gr/unipi/javaspot/repositories/ChapterRepository.java
@@ -1,11 +1,15 @@
 package gr.unipi.javaspot.repositories;
 
+import gr.unipi.javaspot.enums.SkillLevel;
 import gr.unipi.javaspot.models.Chapter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface ChapterRepository extends JpaRepository<Chapter, Integer> {
+
     List<Chapter> findByPrerequisiteId(int prerequisiteId);
+
+    List<Chapter> findChaptersBySkillLevelLessThanEqual(SkillLevel skillLevel);
+
 }
