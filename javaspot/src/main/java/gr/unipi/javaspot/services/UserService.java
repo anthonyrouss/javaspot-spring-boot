cd43684a6f7135120c3d702874e0bc629c9cc1e1
Anthony Roussos <anthony.rouss@proton.me>
2024-07-23 15:36:52 +0300

    Update UserService.java

--- javaspot/src/main/java/gr/unipi/javaspot/services/UserService.java
+++ javaspot/src/main/java/gr/unipi/javaspot/services/UserService.java
@@ -1,11 +1,16 @@
 package gr.unipi.javaspot.services;
 
 import gr.unipi.javaspot.dtos.UserDto;
+import gr.unipi.javaspot.models.Chapter;
+import gr.unipi.javaspot.models.User;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
 
     void signUpAsStudent(UserDto userDto);
-
     boolean usernameExists(String username);
-
+    User findByUsername(String username);
+    void unlockChapters(String username, List<Chapter> chapters);
 }
