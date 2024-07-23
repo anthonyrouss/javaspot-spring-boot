712424f34f7bfa6ba2543b7362e5a0ebbdfe51f8
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-23 19:14:50 +0300

    Update UserRepository.java

--- javaspot/src/main/java/gr/unipi/javaspot/repositories/UserRepository.java
+++ javaspot/src/main/java/gr/unipi/javaspot/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package gr.unipi.javaspot.repositories;
 
+import gr.unipi.javaspot.models.Chapter;
 import gr.unipi.javaspot.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    boolean existsByUnlockedChaptersContainsAndId(Chapter chapter, Integer id);
 
 }
