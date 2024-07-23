72913366aee0b460b4870d6a0cabb80632abac74
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-23 14:47:52 +0300

    Rename files
    - ChapterDTO.java to ChapterDto.java.
    - QuestionDTO.java to QuestionDto.java.

--- javaspot/src/main/java/gr/unipi/javaspot/services/ChapterService.java
+++ javaspot/src/main/java/gr/unipi/javaspot/services/ChapterService.java
@@ -1,11 +1,11 @@
 package gr.unipi.javaspot.services;
 
-import gr.unipi.javaspot.dtos.ChapterDTO;
+import gr.unipi.javaspot.dtos.ChapterDto;
 import gr.unipi.javaspot.models.Chapter;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChapterService extends GeneralService<Chapter> {
-    List<ChapterDTO> getNextChapters(int prerequisiteId);
+    List<ChapterDto> getNextChapters(int prerequisiteId);
 }
