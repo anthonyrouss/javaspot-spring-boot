41aec1b887513758a3720cf11bb0cf1d6121075c
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-23 19:14:29 +0300

    Update QuestionService.java

--- javaspot/src/main/java/gr/unipi/javaspot/services/QuestionService.java
+++ javaspot/src/main/java/gr/unipi/javaspot/services/QuestionService.java
@@ -2,8 +2,6 @@ package gr.unipi.javaspot.services;
 
 import gr.unipi.javaspot.dtos.SkillLevelQuestion;
 
-import java.util.List;
-
 public interface QuestionService {
 
     SkillLevelQuestion[] getSkillLevelAssignmentQuestions();
