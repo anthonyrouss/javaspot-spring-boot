06b8aae35c9769cd90c88551a233f58a4911e8a8
Anthony Roussos <anthony.rouss@proton.me>
2024-07-23 15:33:48 +0300

    Update ChapterController.java
    - Replace @AllArgsConstructor with @RequiredArgsConstructor
    - Update getChapters method
    - Update getChapterSections method

--- javaspot/src/main/java/gr/unipi/javaspot/controllers/ChapterController.java
+++ javaspot/src/main/java/gr/unipi/javaspot/controllers/ChapterController.java
@@ -1,38 +1,66 @@
 package gr.unipi.javaspot.controllers;
 
+import gr.unipi.javaspot.dtos.ChapterAvailability;
 import gr.unipi.javaspot.enums.ExamStatus;
 import gr.unipi.javaspot.exceptions.ChapterNotFoundException;
+import gr.unipi.javaspot.exceptions.InvalidAction;
 import gr.unipi.javaspot.models.Chapter;
 import gr.unipi.javaspot.models.Exam;
+import gr.unipi.javaspot.models.User;
 import gr.unipi.javaspot.services.ChapterService;
 import gr.unipi.javaspot.services.ExamService;
 import gr.unipi.javaspot.services.SectionService;
 import gr.unipi.javaspot.enums.ControllerConstants;
-import lombok.AllArgsConstructor;
+import gr.unipi.javaspot.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ChapterController {
 
     private final ChapterService chapterService;
     private final SectionService sectionService;
     private final ExamService examService;
+    private final UserService userService;
 
     @GetMapping({"/", "/index", "/chapters"})
-    public String getChapters(Model model) {
-        model.addAttribute("chapters", chapterService.findAll());
+    public String getChapters(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        List<Chapter> chapters = chapterService.findAll();
+
+        List<ChapterAvailability> chapterAvailabilityList = new ArrayList<>();
+
+        chapters.forEach(chapter -> {
+            ChapterAvailability chapterAvailability = new ChapterAvailability(
+                    chapter.toDto(),
+                    user.getUnlockedChapters().contains(chapter)
+            );
+            chapterAvailabilityList.add(chapterAvailability);
+        });
+
+        model.addAttribute("chapterAvailabilityList", chapterAvailabilityList);
         return ControllerConstants.CHAPTERS.getHtmlFilePath();
     }
 
     @GetMapping("/chapters/{id}/sections")
-    public String getChapterSections(Model model, @PathVariable int id) {
+    public String getChapterSections(Model model, @PathVariable int id, Principal principal) {
         Chapter selectedChapter = chapterService.getById(id).orElseThrow(() -> new ChapterNotFoundException(id));
+        User user = userService.findByUsername(principal.getName());
+
+        boolean userHasAccess = user.getUnlockedChapters().stream()
+                .map(Chapter::getId)
+                .anyMatch(chapterId -> chapterId.equals(selectedChapter.getId()));
+
+        if (!userHasAccess) throw new InvalidAction("The chapter is locked");
+
         model.addAttribute("selectedChapterTitle", selectedChapter.getTitle());
         model.addAttribute("sections", sectionService.getAllByChapterId(id));
         return ControllerConstants.SECTIONS.getHtmlFilePath();
