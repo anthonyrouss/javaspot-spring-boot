6a7e553b30f9de9477946f9bc96b270e7b1f9b4f
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-23 14:50:59 +0300

    Update ChapterService.java

--- javaspot/src/main/java/gr/unipi/javaspot/services/ChapterService.java
+++ javaspot/src/main/java/gr/unipi/javaspot/services/ChapterService.java
@@ -4,7 +4,6 @@ import gr.unipi.javaspot.dtos.ChapterDto;
 import gr.unipi.javaspot.models.Chapter;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ChapterService extends GeneralService<Chapter> {
     List<ChapterDto> getNextChapters(int prerequisiteId);
