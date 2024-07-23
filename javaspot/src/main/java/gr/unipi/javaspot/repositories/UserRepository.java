5dfbab10656d04dda717755fb27c79fa986e386b
Anthony Roussos <anthony.rouss@proton.me>
2024-07-23 15:35:24 +0300

    Replace User with Optional<User>

--- javaspot/src/main/java/gr/unipi/javaspot/repositories/UserRepository.java
+++ javaspot/src/main/java/gr/unipi/javaspot/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package gr.unipi.javaspot.repositories;
 import gr.unipi.javaspot.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
 
