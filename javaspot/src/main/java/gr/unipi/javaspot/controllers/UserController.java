9f7fe459207ccf4f0c0f6457b48fcdb6030f9300
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-07-23 15:23:23 +0300

    Update UserController.java

--- javaspot/src/main/java/gr/unipi/javaspot/controllers/UserController.java
+++ javaspot/src/main/java/gr/unipi/javaspot/controllers/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static gr.unipi.javaspot.security.SecurityConfig.getUserDetails;
